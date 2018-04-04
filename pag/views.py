c5ceb9aa0477aa5d5298edc24216f6229dbd0b57
Christian9324 <c3_fireblade@hotmail.com>
2018-04-04 11:30:23 -0500

    Modifique el Template, views y el html

--- pag/views.py
+++ pag/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-	return render(request, 'pag/post_list.html', {})
+	posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).order_by('fecha_publicado')
+	return render(request, 'pag/post_list.html', {'posts':posts})
 
 # Create your views here.
