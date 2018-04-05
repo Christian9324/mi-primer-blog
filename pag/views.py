f6ab168017aa12e04001abbcdefeb352dc598b44
Christian9324 <c3_fireblade@hotmail.com>
2018-04-05 16:06:50 -0500

    Se agrego paginas indiv para cada post y se mejoro el diseño

--- pag/views.py
+++ pag/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
 	posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).order_by('fecha_publicado')
 	return render(request, 'pag/post_list.html', {'posts':posts})
+
+def post_detalles(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'pag/post_detalles.html', {'post': post})
 
 # Create your views here.
